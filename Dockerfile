cafab71ccaca2aeb5a213190041e5dcb88d7807c
ossiemarks <osmanmarks@icloud.com>
2016-02-02 04:55:32 +0000

    udpated all files

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,13 @@ ENV LANG C.UTF-8
 
 RUN apt-get update; apt-get install -y \
     apache2 \
+    bash \
     openssl
 
 RUN rm -rf /var/www/html/*; rm -rf /etc/apache2/sites-enabled/*; \
-    mkdir -p /etc/apache2/external
+    mkdir -p /etc/apache2/external; \
+    mkdir -p /u01/app/myCA/ ; \
+    mkdir -p /var/www/html/secure/
 
 ENV APACHE_RUN_USER www-data
 ENV APACHE_RUN_GROUP www-data
@@ -19,18 +22,51 @@ ENV APACHE_LOG_DIR /var/log/apache2
 RUN sed -i 's/^ServerSignature/#ServerSignature/g' /etc/apache2/conf-enabled/security.conf; \
     sed -i 's/^ServerTokens/#ServerTokens/g' /etc/apache2/conf-enabled/security.conf; \
     echo "ServerSignature Off" >> /etc/apache2/conf-enabled/security.conf; \
-    echo "ServerTokens Prod" >> /etc/apache2/conf-enabled/security.conf; \
+    echo "ServerTokens Prod" >> /etc/apache2/conf-enabled/security.conf; \ 
+    sed -i "s|./demoCA|/u01/app/myCA/|g" /etc/ssl/openssl.cnf; \
+    echo " modified openssl.cnf"; \
     a2enmod ssl; \
     a2enmod headers; \
     echo "SSLProtocol ALL -SSLv2 -SSLv3" >> /etc/apache2/apache2.conf
 
 ADD 000-default.conf /etc/apache2/sites-enabled/000-default.conf
 ADD 001-default-ssl.conf /etc/apache2/sites-enabled/001-default-ssl.conf
+ADD openssl.cnf /etc/openssl/openssl.cnf
+ADD index.html /var/www/html/secure/index.html
 
 EXPOSE 80
 EXPOSE 443
 
 ADD entrypoint.sh /opt/entrypoint.sh
+ADD openssl.cnf /u01/app/myCA/
+ADD create_keys.sh /opt/create_keys.sh
+
+RUN mkdir -p /u01/app/myCA/certs; \
+mkdir /u01/app/myCA/csr; \
+mkdir /u01/app/myCA/newcerts; \
+mkdir /u01/app/myCA/private; \
+cp /etc/openssl/openssl.cnf /u01/app/myCA/.
+
+WORKDIR /u01/app/myCA 
+
+RUN echo 00 > serial ;\
+echo 00 > crlnumber ;\
+touch index.txt
+
+
+RUN openssl genrsa -des3 -passout pass:qwerty -out  private/rootCA.key 2048; \
+openssl rsa -passin pass:qwerty -in private/rootCA.key -out private/rootCA.key; \
+openssl req -config openssl.cnf -new -x509 -subj '/C=DK/L=Aarhus/O=mariocart CA/CN=apache-ssl' -days 999 -key private/rootCA.key -out certs/rootCA.crt; \
+openssl genrsa -des3 -passout pass:qwerty -out private/winterfell.key 2048; \
+openssl rsa -passin pass:qwerty -in private/winterfell.key -out private/winterfell.key; \
+openssl req -config openssl.cnf -new -subj '/C=DK/L=Aarhus/O=mariocart/CN=winterfell' -key private/winterfell.key -out csr/winterfell.csr; \
+openssl ca -batch -config openssl.cnf -days 999 -in csr/winterfell.csr -out certs/winterfell.crt -keyfile private/rootCA.key -cert certs/rootCA.crt -policy policy_anything; \
+openssl genrsa -des3 -passout pass:qwerty -out private/client.key 2048; \
+openssl rsa -passin pass:qwerty -in private/client.key -out private/client.key; \
+openssl req -config openssl.cnf -new -subj '/C=DK/L=Aarhus/O=mariocart/CN=theClient' -key private/client.key -out csr/client.csr; \
+openssl ca -batch -config openssl.cnf -days 999 -in csr/client.csr -out certs/client.crt -keyfile private/rootCA.key -cert certs/rootCA.crt -policy policy_anything; \
+openssl pkcs12 -export -passout pass:qwerty -in certs/client.crt -inkey private/client.key -certfile certs/rootCA.crt -out certs/clientcert.p12 
+
 RUN chmod a+x /opt/entrypoint.sh
 
 ENTRYPOINT ["/opt/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ If you want to add your own VirtualHosts Configuration, you can copy the followi
 	SSLCipherSuite ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!CAMELLIA:!DES:!MD5:!PSK:!RC4
 	SSLHonorCipherOrder on
 
-	<Directory "/var/www/html/">
+	<Directory "/var/www/html/secure/">
 	SSLCertificateFile /u01/app/myCA/certs/winterfell.crt
 	SSLCertificateKeyFile /u01/app/myCA/private/winterfell.key
 	SSLCertificateChainFile /u01/app/myCA/certs/rootCA.crt
@@ -45,46 +45,13 @@ else
   echo ">> HSTS Headers disabled"
 fi
 
-  echo ">> generating self signed cert"
-	mkdir -p /u01/app/myCA/certs
-	mkdir /u01/app/myCA/csr
-	mkdir /u01/app/myCA/newcerts
-	mkdir /u01/app/myCA/private
-	cd /u01/app/myCA
-	echo 00 > serial
-	echo 00 > crlnumber
-	touch index.txt
-	# Create CA private key
-	openssl genrsa -des3 -passout pass:qwerty -out  private/rootCA.key 2048
-	# Remove passphrase
-	openssl rsa -passin pass:qwerty -in private/rootCA.key -out private/rootCA.key
-	# Create CA self-signed certificate
-	openssl req -config openssl.cnf -new -x509 -subj '/C=DK/L=Aarhus/O=frogger CA/CN=theheat.dk' -days 999 -key private/rootCA.key -out certs/rootCA.crt
-	# Create private key for the winterfell server
-	openssl genrsa -des3 -passout pass:qwerty -out private/winterfell.key 2048
-	# Remove passphrase
-	openssl rsa -passin pass:qwerty -in private/winterfell.key -out private/winterfell.key
-	# Create CSR for the winterfell server
-	openssl req -config openssl.cnf -new -subj '/C=DK/L=Aarhus/O=frogger/CN=winterfell' -key private/winterfell.key -out csr/winterfell.csr
-	# Create certificate for the winterfell server
-	openssl ca -batch -config openssl.cnf -days 999 -in csr/winterfell.csr -out certs/winterfell.crt -keyfile private/rootCA.key -cert certs/rootCA.crt -policy policy_anything
-	# Create private key for a client
-	openssl genrsa -des3 -passout pass:qwerty -out private/client.key 2048
-	# Remove passphrase
-	openssl rsa -passin pass:qwerty -in private/client.key -out private/client.key
-	# Create CSR for the client.
-	openssl req -config openssl.cnf -new -subj '/C=DK/L=Aarhus/O=frogger/CN=theClient' -key private/client.key -out csr/client.csr
-	# Create client certificate.
-	openssl ca -batch -config openssl.cnf -days 999 -in csr/client.csr -out certs/client.crt -keyfile private/rootCA.key -cert certs/rootCA.crt -policy policy_anything
-	# Export the client certificate to pkcs12 for import in the browser
-	openssl pkcs12 -export -passout pass:qwerty -in certs/client.crt -inkey private/client.key -certfile certs/rootCA.crt -out certs/clientcert.p12
  echo ">> Copying client certs to /tmp"
-cp 	certs/rootCA.crt certs/clientcert.p12 /tmp/
+cp /u01/app/myCA/certs/rootCA.crt /u01/app/myCA/certs/clientcert.p12 /var/www/
 
 
 echo ">> copy /etc/apache2/external/*.conf files to /etc/apache2/sites-enabled/"
 cp /etc/apache2/external/*.conf /etc/apache2/sites-enabled/ 2> /dev/null > /dev/null
-
+#sed "s|./demoCA|/u01/app/myCA/|g" /etc/ssl/openssl.cnf
 # exec CMD
 echo ">> exec docker CMD"
 echo "$@"
